910dc1815de5cff9ae41e8545798600a2b717dcb
Fazal Majid <fazal@majid.org>
2020-11-10 16:07:29 -0800

    fix sort order for snapshots, otherwise destroying foo@daily-2020-11-10 before foo/bar@daily-202-11-10 would cause the latter to fail because it was already destroyed

--- zfssnap.go
+++ zfssnap.go
@@ -12,11 +12,20 @@ import (
 	"time"
 )
 
-const date_fmt = "[02/Jan/2006:15:04:05 -0700]"
-const iso_8601 = "2006-01-02 15:04:05"
-const RETENTION = 14 // retention period in days
+const (
+	date_fmt  = "[02/Jan/2006:15:04:05 -0700]"
+	iso_8601  = "2006-01-02 15:04:05"
+	RETENTION = 14 // retention period in days
+)
+
+var (
+	verbose *bool
+)
 
 func create(snap string) {
+	if *verbose {
+		log.Println("creating ", snap)
+	}
 	zfs := exec.Command("/usr/sbin/zfs", "snapshot", "-r", snap)
 	zfs.Stdout = os.Stdout
 	zfs.Stderr = os.Stderr
@@ -26,11 +35,31 @@ func create(snap string) {
 }
 
 func destroy(snap string) {
+	if *verbose {
+		log.Println("destroying ", snap)
+	}
 	zfs := exec.Command("/usr/sbin/zfs", "destroy", "-r", snap)
 	zfs.Stdout = os.Stdout
 	zfs.Stderr = os.Stderr
 	if err := zfs.Run(); err != nil {
 		log.Fatal("error destroying snapshot ", snap, ": ", err)
+	}
+}
+
+type Snaps []string
+
+func (a Snaps) Len() int      { return len(a) }
+func (a Snaps) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a Snaps) Less(i, j int) bool {
+	si := strings.SplitN(a[i], "@", 2)
+	sj := strings.SplitN(a[j], "@", 2)
+	switch {
+	case si[0] < sj[0]:
+		return true
+	case si[0] > sj[0]:
+		return false
+	default:
+		return si[1] < sj[1]
 	}
 }
 
@@ -58,7 +87,6 @@ func destroy_old_snaps(datasets []string, snaps map[string]bool) {
 			continue
 		}
 		snaps[snapshot] = true
-		//fmt.Println(snapshot)
 		i := strings.Index(snapshot, "@daily-")
 		if i != -1 {
 			date, err := time.Parse("@daily-2006-01-02", snapshot[i:])
@@ -72,15 +100,14 @@ func destroy_old_snaps(datasets []string, snaps map[string]bool) {
 	if err := zfs.Wait(); err != nil {
 		log.Fatal("error waiting to exit: ", err)
 	}
-	sort.Strings(todo)
-	for i := range todo {
-		snapshot := todo[len(todo)-1-i]
-		//fmt.Println("destroying", snapshot)
+	sort.Sort(sort.Reverse(Snaps(todo)))
+	for _, snapshot := range todo {
 		destroy(snapshot)
 	}
 }
 
 func main() {
+	verbose = flag.Bool("v", false, "Verbose logging")
 	flag.Parse()
 	datasets := flag.Args()
 	snaps := make(map[string]bool)
@@ -89,18 +116,14 @@ func main() {
 
 	now := time.Now()
 	for _, fs := range datasets {
-		//fmt.Println(i, " ", fs)
 		hourly := fmt.Sprintf("%s@%s", fs, now.Format("hourly-15"))
 		if snaps[hourly] {
-			//fmt.Println("destroying", hourly)
 			destroy(hourly)
 		}
-		//fmt.Println("creating", hourly)
 		create(hourly)
 
 		daily := fmt.Sprintf("%s@%s", fs, now.Format("daily-2006-01-02"))
 		if !snaps[daily] {
-			//fmt.Println("creating", daily)
 			create(daily)
 		}
 	}
